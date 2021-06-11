b32b14348fd224080aad31a73f0600b5a6586d9b
Richard Gibert <richard@gibert.ca>
2021-06-10 21:25:58 -0400

    remove unneeded escaping

--- passwordgen.sh
+++ passwordgen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-openssl rand -base64 32 | tr \!@\#$%^\&\*\(\)+=\\\/\“\' _ | tr Oo 0 | tr Ii 1 | tr B 8
+openssl rand -base64 32 | tr \!@\#$%^\&\*\(\)+=\\/\"\' _ | tr Oo 0 | tr Ii 1 | tr B 8
